f0cc6a1c5558a5861a8a4477a742148c6833456e
PREMIUM PROJECTS <daitoroben@gmail.com>
2026-06-07 11:11:41 +0100

    Add HIRE A WRITER button to header

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,5 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 export default defineConfig({
-  legacy: {
-    collectionsBackwardsCompat: true,
-  },
+  output: 'static',
 });
